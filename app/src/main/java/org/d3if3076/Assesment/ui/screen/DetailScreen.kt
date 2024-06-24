ba082e519ecc4b8cd05fa2e33a523e0fecde4204
Reiko Alaudin Rasyid <reyalrsyd@gmail.com>
2024-06-24 16:54:19 +0700

    Assesmen 2_Rev

--- app/src/main/java/org/d3if3076/Assesment/ui/screen/DetailScreen.kt
+++ app/src/main/java/org/d3if3076/Assesment/ui/screen/DetailScreen.kt
@@ -2,8 +2,6 @@ package org.d3if3076.Assesment.ui.screen
 
 import android.content.res.Configuration
 import android.widget.Toast
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -39,7 +36,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -190,7 +186,6 @@ fun FormMahasiswa(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(150.dp)
-                .border(BorderStroke(width = 1.dp, color = Color.Black), shape = RoundedCornerShape(4.dp))
                 .padding(16.dp)
         ) {
             Column {
